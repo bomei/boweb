ac984c4dc21dbb2acf910614c7ed67adb63007ee
Bo Zhang <zannb@outlook.com>
2017-05-10 17:03:31 +0800

    增加心跳。mongo auth没搞明白，很僵化

--- tornado_app/base.py
+++ tornado_app/base.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-import tornado.web
 import motor
+import tornado.web
 
 dbClient = motor.motor_tornado.MotorClient('zannb.site', 27017)
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -11,3 +12,14 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def get_current_xsrf(self):
         return self.get_secure_cookie('_xsrf')
+
+    @staticmethod
+    async def db_login(c, db, username, pwd):
+        await c[db].authenticate(username, pwd)
+
+    @staticmethod
+    async def init():
+        # asyncio.ensure_future(BaseHandler.db_login(dbClient, 'Tornado', 'tornado', 'tornado:mongo'))
+        # asyncio.ensure_future(BaseHandler.db_login(dbClient,'admin','bobo','bobo:mongo'))
+        await BaseHandler.db_login(dbClient, 'Tornado', 'tornado', 'tornado:mongo')
+        await BaseHandler.db_login(dbClient, 'admin', 'bobo', 'bobo:mongo')
